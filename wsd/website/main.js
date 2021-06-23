f7e67a0a986225659d2579c020d426d1ad112f93
dklgarima <garimagorkhali@gmail.com>
2021-06-23 20:46:14 +0545

    Login page added

--- wsd/website/main.js
+++ wsd/website/main.js
@@ -87,3 +87,31 @@ slider.addEventListener("mouseout" , ()=> {
         showslide();   
     } , 3000);    // on mouseout from slide then again start Changing every image after 3  seconds 
 });
+
+//login/signup
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+//password toggle
+var state= false;
+function toggle(){
+    if(state){
+	document.getElementById("password").setAttribute("type","password");
+	document.getElementById("eye").style.color='#7a797e';
+	state = false;
+     }
+     else{
+	document.getElementById("password").setAttribute("type","text");
+	document.getElementById("eye").style.color='#5887ef';
+	state = true;
+     }
+}
